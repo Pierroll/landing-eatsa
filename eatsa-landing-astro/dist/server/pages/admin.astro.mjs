83cc92eb7653c730e5e66002f342d68dd63ed0ab
Pierol <pierol@MacBook-Air-2.local>
2026-06-30 13:59:21 -0500

    chore: UX/UI audit adjustments and final polish

--- eatsa-landing-astro/dist/server/pages/admin.astro.mjs
+++ eatsa-landing-astro/dist/server/pages/admin.astro.mjs
@@ -1,7 +1,7 @@
 /* empty css                                 */
 import { c as createComponent, r as renderComponent, d as renderTemplate, m as maybeRenderHead } from '../chunks/astro/server_Bg-g91To.mjs';
 import 'kleur/colors';
-import { $ as $$AdminLayout } from '../chunks/AdminLayout_CH-801Fc.mjs';
+import { $ as $$AdminLayout } from '../chunks/AdminLayout_C-CbQ7NA.mjs';
 export { renderers } from '../renderers.mjs';
 
 const prerender = false;
